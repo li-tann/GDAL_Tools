10a7fa7fbb060e5927d64f23c64425b292b841e0
litan_thinkPad <402512849@qq.com>
2023-12-25 17:16:36 +0800

    init goldstein plus function

---
+++ goldstein_plus.cpp
@@ -0,0 +1,4 @@
+/// TODO:
+/// baran & zhao filter base on goldstein
+/// diff point with goldstein is input a correlation map (baran is coherence map, zhao is pseudo coherence map)
+/// and it will change to the zhao filter automatically is the input of correlation map is unexisted.
